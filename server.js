3c15c5d79ba2389d5a023e67863bbe81c512b3b6
Adam Burch <rewop360@gmail.com>
2021-06-24 09:29:55 -0700

    get /api/notes works

--- server.js
+++ server.js
@@ -15,18 +15,13 @@ const PORT = 8080;
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 app.get("/notes", (req, res) => res.sendFile(__dirname + "/notes.html"));
 
-let db;
-
 app.get("/api/notes", (req,res) => {
     console.log("/api/notes getting da notes");
-    db = fs.readFile(__dirname + "/db/db.json", "utf8", 
+    fs.readFile(__dirname + "/db/db.json", "utf8", 
     (err, data) => {
         console.log(err);
-        db = data; 
+        res.json(data)
     });
-
-    console.log(db);
-    res.json(db)
 });
 
 app.listen(PORT, () => console.log("the app can hear"));
