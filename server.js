b46d6e9904926e54c927c0d90e963ce8d6d7876d
Adam Burch <rewop360@gmail.com>
2021-06-23 17:33:13 -0700

    /api/notes GET not working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 // Parses the body of the request
@@ -14,6 +15,18 @@ const PORT = 8080;
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 app.get("/notes", (req, res) => res.sendFile(__dirname + "/notes.html"));
 
-//app.get()
+let db;
+
+app.get("/api/notes", (req,res) => {
+    console.log("/api/notes getting da notes");
+    db = fs.readFile(__dirname + "/db/db.json", "utf8", 
+    (err, data) => {
+        console.log(err);
+        db = data; 
+    });
+
+    console.log(db);
+    res.json(db)
+});
 
 app.listen(PORT, () => console.log("the app can hear"));
